9f95d8d50f0ddaae055bbbdfe6cb315d5918909b
UnalignedByte <rafal@unalignedbyte.com>
2016-02-09 19:47:53 +0000

    Initial commit

---
+++ Kulki/Source/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  Kulki
+//
+//  Created by Rafal Grodzinski on 31/01/16.
+//  Copyright © 2016 UnalignedByte. All rights reserved.
+//
+
+import UIKit
+
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate
+{
+    var window: UIWindow?
+
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool
+    {
+        return true
+    }
+}
+
---
+++ Kulki/Source/GameViewController.swift
@@ -0,0 +1,14 @@
+//
+//  GameViewController.swift
+//  Kulki
+//
+//  Created by Rafal Grodzinski on 09/02/16.
+//  Copyright © 2016 UnalignedByte. All rights reserved.
+//
+
+import UIKit
+
+
+class GameViewController: UIViewController
+{
+}
