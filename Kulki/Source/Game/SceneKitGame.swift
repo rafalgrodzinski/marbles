d5f4e604d81c31852cf089fa16062397c44fe290
UnalignedByte <rafal@unalignedbyte.com>
2016-04-26 23:50:11 +0100

    Added a single 3D tile

--- Kulki/Source/Game/SceneKitGame.swift
+++ Kulki/Source/Game/SceneKitGame.swift
@@ -28,13 +28,30 @@ class SceneKitGame: Game
         (self.view as! SCNView).autoenablesDefaultLighting = true
         (self.view as! SCNView).allowsCameraControl = true
 
-        let a = SCNNode()
+        /*let a = SCNNode()
         a.geometry = SCNBox(width: 1.0, height: 1.0, length: 1.0, chamferRadius: 0.1)
-        self.scene.rootNode.addChildNode(a)
+        self.scene.rootNode.addChildNode(a)*/
     }
 
 
     override func showBoard(finished: () -> Void)
     {
+        for y in 0 ..< field.size.height {
+            for x in 0 ..< field.size.width {
+                let tile = SCNNode()
+                tile.geometry = SCNPlane(width: 1.0, height: 1.0)
+                let tileMaterial = SCNMaterial()
+                tileMaterial.diffuse.contents = UIImage(named: "Tile")
+                tileMaterial.normal.contents = UIImage(named: "Tile")
+                tile.geometry!.firstMaterial = tileMaterial
+
+                /*let tile = SKSpriteNode(imageNamed: "Tile")
+                tile.size = self.tileSize
+                tile.position = self.positionForFieldPosition(Point(x, y))!
+
+                self.scene.addChild(tile)*/
+                self.scene.rootNode.addChildNode(tile)
+            }
+        }
     }
 }
