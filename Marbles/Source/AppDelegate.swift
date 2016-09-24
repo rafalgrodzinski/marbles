6ebeb8fa88c239013663e614039ee68f682cae43
Rafal Grodzinski <rafal@EvilBook.local>
2016-09-24 18:26:42 +0100

    Added Frabric & Crashlytics

--- Marbles/Source/AppDelegate.swift
+++ Marbles/Source/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Fabric
+import Crashlytics
 
 
 @UIApplicationMain
@@ -17,6 +19,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool
     {
+        Fabric.with([Crashlytics.self])
+
         return true
     }
 }
