714e4d2daf2e7e3792e62e6696cc614cd444591e
Rafal Grodzinski <rafal@EvilBook.local>
2016-09-25 12:57:49 +0100

    Added tip jar button and incomplete screen

---
+++ Marbles/Source/Main Menu/TipJarViewController.swift
@@ -0,0 +1,17 @@
+//
+//  TipJarViewController.swift
+//  Marbles
+//
+//  Created by Rafal Grodzinski on 25/09/2016.
+//  Copyright © 2016 UnalignedByte. All rights reserved.
+//
+
+import UIKit
+
+class TipJarViewController: UIViewController
+{
+    @IBAction private func menuButtonPressed(_ sender: UIButton)
+    {
+        self.dismiss(animated: true, completion: nil)
+    }
+}
