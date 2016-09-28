c15ecc6edb9a55e1d5df4d881e1d51e86a8d95e1
Rafal Grodzinski <rafal@EvilBook.local>
2016-09-28 09:16:31 +0100

    Added purchase manager and fixed up tip jar screen

--- Marbles/Source/Main Menu/TipJarViewController.swift
+++ Marbles/Source/Main Menu/TipJarViewController.swift
@@ -7,11 +7,83 @@
 //
 
 import UIKit
+import StoreKit
 
-class TipJarViewController: UIViewController
+
+class TipJarViewController: UIViewController, UITableViewDataSource, UITableViewDelegate
 {
+    @IBOutlet private var menuButton: UIButton!
+    @IBOutlet private var tableView: UITableView!
+
+    private var products: [SKProduct]?
+
+
+    override func viewDidLoad()
+    {
+        PurchaseManager.sharedInstance.fetchProducts() { (products: [SKProduct]) in
+            self.products = products
+            self.tableView.reloadData()
+        }
+    }
+
+
     @IBAction private func menuButtonPressed(_ sender: UIButton)
     {
         self.dismiss(animated: true, completion: nil)
+    }
+
+
+    // MARK: UITableView Delegate
+    func numberOfSections(in tableView: UITableView) -> Int
+    {
+        return 1
+    }
+
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        let rowsCount = self.products != nil ? self.products!.count : 0
+
+        return rowsCount
+    }
+
+
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat
+    {
+        return 72.0
+    }
+
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let product = self.products?[indexPath.row]
+
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TipCell", for: indexPath)
+        cell.backgroundColor = UIColor.marblesGreen().withAlphaComponent(0.4)
+        cell.layer.cornerRadius = 5.0
+        cell.layer.borderColor = UIColor.white.cgColor
+        cell.layer.borderWidth = 1.0
+
+        let titleLabel = cell.viewWithTag(1)
+        if let title = titleLabel as? UILabel {
+            title.font = UIFont(name: "BunakenUnderwater", size: 24.0)
+            title.text = "My Title"//product?.localizedTitle
+        }
+
+        let priceLabel = cell.viewWithTag(2)
+        if let price = priceLabel as? UILabel {
+            let formatter = NumberFormatter()
+            formatter.numberStyle = .currency
+            formatter.locale = Locale(identifier: "gb")// product?.priceLocale
+            price.font = UIFont(name: "BunakenUnderwater", size: 20.0)
+            price.text = formatter.string(from: 1.0)//product!.price)
+        }
+
+        let tipButton = cell.viewWithTag(3)
+        if let tip = tipButton as? UIButton {
+            tip.titleLabel?.font = UIFont(name: "BunakenUnderwater", size: 28.0)
+        }
+
+        return cell
     }
 }
---
+++ Marbles/Source/PurchaseManager.swift
@@ -0,0 +1,91 @@
+//
+//  PurchaseManager.swift
+//  Marbles
+//
+//  Created by Rafal Grodzinski on 25/09/2016.
+//  Copyright © 2016 UnalignedByte. All rights reserved.
+//
+
+import Foundation
+import StoreKit
+
+
+class PurchaseManager: NSObject, SKProductsRequestDelegate, SKPaymentTransactionObserver
+{
+    static let sharedInstance = PurchaseManager()
+
+    private var productsRequest: SKProductsRequest?
+    private let productIdentifiers: Set<String> = []
+
+    private var products: [SKProduct]?
+    private var fetchProductsCallback: (([SKProduct]) -> Void)?
+    private var buyProductCallback: ((Bool) -> Void)?
+
+
+    private override init()
+    {
+        super.init()
+
+        SKPaymentQueue.default().add(self)
+
+        var p = SKProduct()
+
+        var p2 = SKProduct()
+
+        self.products = [p, p2]
+    }
+
+
+    func fetchProducts(_ completed: @escaping ([SKProduct]) -> Void)
+    {
+        self.productsRequest?.cancel()
+
+        if let products = self.products {
+            completed(products)
+            return
+        }
+
+        if !SKPaymentQueue.canMakePayments() {
+            completed([])
+            return
+        }
+
+        self.fetchProductsCallback = completed
+
+        self.productsRequest = SKProductsRequest(productIdentifiers: self.productIdentifiers)
+        self.productsRequest?.delegate = self
+        self.productsRequest?.start()
+    }
+
+
+    func buyProduct(_ product: SKProduct, completed: @escaping (Bool) -> Void)
+    {
+        self.buyProductCallback = completed
+
+        let payment = SKPayment(product: product)
+        SKPaymentQueue.default().add(payment)
+    }
+
+
+    // MARK: Products Request Delegate
+    func productsRequest(_ request: SKProductsRequest, didReceive response: SKProductsResponse)
+    {
+        self.products = response.products
+        self.fetchProductsCallback?(response.products)
+    }
+
+
+    func paymentQueue(_ queue: SKPaymentQueue, updatedTransactions transactions: [SKPaymentTransaction])
+    {
+        for transaction in transactions {
+            switch transaction.transactionState {
+            case .purchased:
+                self.buyProductCallback?(true)
+            case .failed:
+                self.buyProductCallback?(false)
+            default:
+                break
+            }
+        }
+    }
+}
