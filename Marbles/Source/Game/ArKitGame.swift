ff730d22577d8f8fa71e583d1d945c2e6294a3e8
Rafal Grodzinski <rafal@unalignedbyte.com>
2017-07-04 19:58:40 +0100

    Placeholder is shown before placing the board

--- Marbles/Source/Game/ArKitGame.swift
+++ Marbles/Source/Game/ArKitGame.swift
@@ -12,6 +12,8 @@ import ARKit
 @available(iOS 11.0, *)
 class ArKitGame: SceneKitGame, ARSCNViewDelegate
 {
+    // MARK: - Setup
+    fileprivate let boardWidthInCm: Float = 20.0
     fileprivate let nextMarblesView = SCNView()
     override func setupView()
     {
@@ -30,30 +32,49 @@ class ArKitGame: SceneKitGame, ARSCNViewDelegate
 
     override func setupScene()
     {
+        gameScale = boardWidthInCm / (100.0 * Float(field.size.width))
+
         super.setupScene()
+        setupPlaceholder()
 
         self.view.backgroundColor = UIColor.clear
         self.view.superview?.subviews.filter { $0 is MainMenuBackgroundView }.first?.removeFromSuperview()
 
-        gameScale = 0.1
+        setupDebug()
+    }
+
+    fileprivate var debugLabel: UILabel!
+    fileprivate func setupDebug()
+    {
+        debugLabel = UILabel()
+        debugLabel.frame = CGRect(x: 0.0, y: 0.0, width: view.frame.width, height: 30.0)
+        debugLabel.textAlignment = .center
+        view.addSubview(debugLabel)
+        debugLabel.text = "Just started"
     }
 
     override func setupCamera()
     {
     }
 
-    func setupCenterNode(with transform: simd_float4x4)
-    {
-        // Setup center node
-        var modelMatrix = SCNMatrix4(simdMatrix: transform)
-        let rotateMatrix = SCNMatrix4MakeRotation(-Float.pi * 0.5, 1.0, 0.0, 0.0) // Make it horizontal
-        modelMatrix = SCNMatrix4Mult(rotateMatrix, modelMatrix)
-        self.centerNode.transform = modelMatrix
 
-        // Setup gravity
-        let gravityForceMatrix = simd_make_float4(0.0, -1.8, 0.0, 0.0)
-        let gravityMatrix = simd_mul(transform, gravityForceMatrix)
-        self.scene.physicsWorld.gravity = SCNVector3(x: gravityMatrix.x, y: gravityMatrix.y, z: gravityMatrix.z)
+    override func setupOverlay()
+    {
+        super.setupOverlay()
+
+        guard let skScene = (view as! SCNView).overlaySKScene else { return }
+
+        // Place Board Button
+        let placeBoardButton = Button(defaultTexture: SKTexture(imageNamed: "Menu Button"))
+        let center = CGPoint(x: view.frame.midX, y: view.frame.midY - placeBoardButton.size.height * 2.0)
+        placeBoardButton.position = center
+        skScene.addChild(placeBoardButton)
+        placeBoardButton.callback = { [weak self] in
+            placeBoardButton.removeFromParent()
+            self?.setupNextMarbleScene()
+            self?.isStarted = true
+            self?.reallyShowBoard()
+        }
     }
 
     fileprivate func setupNextMarbleScene()
@@ -78,6 +99,17 @@ class ArKitGame: SceneKitGame, ARSCNViewDelegate
         }
     }
 
+    fileprivate func setupPlaceholder()
+    {
+        let placeholderSize = gameScale * Float(field.size.width)
+        let aimPlane = SCNPlane(width: CGFloat(placeholderSize), height: CGFloat(placeholderSize))
+        aimPlane.firstMaterial?.isDoubleSided = true
+        aimPlane.firstMaterial?.diffuse.contents = "Aim"
+        let aimNode = SCNNode(geometry: aimPlane)
+        aimNode.rotation = SCNVector4(x: 0.0, y: 1.0, z: 0.0, w: Float.pi)
+        centerNode.addChildNode(aimNode)
+    }
+
     override func addNextMarble(_ marble: SceneKitMarble)
     {
         self.nextMarblesView.scene?.rootNode.addChildNode(marble.node)
@@ -100,33 +132,43 @@ class ArKitGame: SceneKitGame, ARSCNViewDelegate
     fileprivate var isStarted = false
     override func handleTap(_ sender: UITapGestureRecognizer)
     {
-        if isStarted {
-            super.handleTap(sender)
-        } else if let currentFrame = (self.view as? ARSCNView)?.session.currentFrame {
-            setupNextMarbleScene()
-            setupCenterNode(with: currentFrame.camera.transform)
-            isStarted = true
-            reallyShowBoard()
-        }
+        super.handleTap(sender)
+    }
+
+    func updateCenterNode(with transform: simd_float4x4)
+    {
+        // Setup center node
+        var modelMatrix = SCNMatrix4(simdMatrix: transform)
+        let rotateMatrix = SCNMatrix4MakeRotation(-Float.pi * 0.5, 1.0, 0.0, 0.0) // Make it horizontal
+        modelMatrix = SCNMatrix4Mult(rotateMatrix, modelMatrix)
+        self.centerNode.transform = modelMatrix
+
+        // Setup gravity
+        let gravityForceMatrix = simd_make_float4(0.0, -1.8, 0.0, 0.0)
+        let gravityMatrix = simd_mul(transform, gravityForceMatrix)
+        self.scene.physicsWorld.gravity = SCNVector3(x: gravityMatrix.x, y: gravityMatrix.y, z: gravityMatrix.z)
     }
 
     // MARK: -
     fileprivate var centerNodeAnchor: ARAnchor?
     func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor)
     {
-        if !isStarted {
-            setupNextMarbleScene()
-            centerNodeAnchor = anchor
-            setupCenterNode(with: anchor.transform)
-            isStarted = true
-            reallyShowBoard()
+        DispatchQueue.main.async { [unowned self] in
+            if self.centerNodeAnchor == nil {
+                self.centerNodeAnchor = anchor
+                self.updateCenterNode(with: anchor.transform)
+                self.debugLabel.text = "Renderer added anchor"
+            }
         }
     }
 
     func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor)
     {
         if let centerNodeAnchor = centerNodeAnchor, centerNodeAnchor == anchor {
-            setupCenterNode(with: anchor.transform)
+            DispatchQueue.main.async { [unowned self] in
+                self.debugLabel.text = "Anchor updated"
+                self.updateCenterNode(with: anchor.transform)
+            }
         }
     }
 
@@ -135,6 +177,27 @@ class ArKitGame: SceneKitGame, ARSCNViewDelegate
         if let lightEstimate = (self.view as! ARSCNView).session.currentFrame?.lightEstimate {
             ambientLight.intensity = lightEstimate.ambientIntensity
             spotLight.intensity = lightEstimate.ambientIntensity
+        }
+
+        hitTestNode()
+    }
+
+    fileprivate func hitTestNode()
+    {
+        if isStarted { return }
+
+        DispatchQueue.main.async { [unowned self] in
+            let screenCenter = CGPoint(x: self.view.frame.midX, y: self.view.frame.midY)
+            if let result = (self.view as! ARSCNView).hitTest(screenCenter, types: .featurePoint).first {
+                if let anchor = result.anchor {
+                    self.debugLabel.text = "New Anchor!"
+                    self.centerNodeAnchor = anchor
+                    self.updateCenterNode(with: anchor.transform)
+                } else {
+                    self.debugLabel.text = "New Position!"
+                    self.updateCenterNode(with: result.worldTransform)
+                }
+            }
         }
     }
 }
