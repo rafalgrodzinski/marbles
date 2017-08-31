81e646a7707769183f972f674e7d6e03b618951d
Rafal Grodzinski <rafal@unalignedbyte.com>
2017-08-31 23:18:01 +0100

    Added Mac target

---
+++ Marbles/Mac/Source/AppDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.swift
+//  Marbles
+//
+//  Created by Rafal Grodzinski on 31/08/2017.
+//  Copyright © 2017 UnalignedByte. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+}
---
+++ Marbles/Mac/Source/ViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ViewController.swift
+//  Marbles
+//
+//  Created by Rafal Grodzinski on 31/08/2017.
+//  Copyright © 2017 UnalignedByte. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+}
