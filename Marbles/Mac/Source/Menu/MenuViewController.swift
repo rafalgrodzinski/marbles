971fa9babd14b87517b656d07b28ed2bddc39d4a
Rafal Grodzinski <rafal@unalignedbyte.com>
2018-02-26 19:55:39 +0000

    Fixed crash on macOS 10.12

--- Marbles/Mac/Source/Menu/MenuViewController.swift
+++ Marbles/Mac/Source/Menu/MenuViewController.swift
@@ -92,11 +92,11 @@ class MenuViewController: NSViewController {
         titleShadow.shadowOffset = NSSize(width: 2, height: -2)
         let paragraphStyle = NSMutableParagraphStyle()
         paragraphStyle.alignment = .center
-        let titleAttributes: [NSAttributedStringKey : AnyObject] = [NSAttributedStringKey.font            : NSFont(name: "BunakenUnderwater", size: 28.0)!,
-                                                                    NSAttributedStringKey.foregroundColor : NSColor.white,
-                                                                    NSAttributedStringKey.shadow          : titleShadow,
-                                                                    NSAttributedStringKey.paragraphStyle  : paragraphStyle]
-        button.attributedTitle = NSAttributedString(string: title, attributes: titleAttributes)
+
+        button.attributedTitle = NSAttributedString(string: title, attributes: [.font            : NSFont(name: "BunakenUnderwater", size: 28.0)!,
+                                                                                .foregroundColor : NSColor.white,
+                                                                                .shadow          : titleShadow,
+                                                                                .paragraphStyle  : paragraphStyle])
     }
 
     private func updateHighScoreLabel()
